0d8066e597a988cbc3b9fb9fd059ed759ab49c9b
Nestor Alfaro <nalfarocs@gmail.com>
2024-08-19 22:40:00 +0000

    fixed c++ package not being in the image

--- openspot/package/main/main.mk
+++ openspot/package/main/main.mk
@@ -1,12 +1,12 @@
-OPENSPOT_VERSION = e749ea36803b07b68f20282e00afcc6c34f95941
-OPENSPOT_SITE = git@github.com:nestoralfaro/final-project-assignment-nestoralfaro.git
-OPENSPOT_SITE_METHOD = git
+MAIN_VERSION = e749ea36803b07b68f20282e00afcc6c34f95941
+MAIN_SITE = git@github.com:nestoralfaro/final-project-assignment-nestoralfaro.git
+MAIN_SITE_METHOD = git
 
-define OPENSPOT_BUILD_CMDS
+define MAIN_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
-define OPENSPOT_INSTALL_TARGET_CMDS
+define MAIN_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/openspot $(TARGET_DIR)/usr/bin
 endef
 
